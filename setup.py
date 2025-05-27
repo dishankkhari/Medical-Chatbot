bd991fe8a795c74a289fa3f0c50987ad73bda8a7
dishank khari <dkback2003@gmail.com>
2025-05-28 02:50:03 +0530

    files updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'dishank khari',
+    author_email= 'dkback2003@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
